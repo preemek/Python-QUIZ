307645dbd1acd317aff6cb4205f572ed5f7c9213
YTGhosti25 <osmy149@gmail.com>
2025-03-02 08:12:06 +0100

    changet main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQuiz import QuizGUI
 import tkinter as tk
 
-root=tk.Tk()
-game=QuizGUI(root)
-root.mainloop()
+if __name__ == "__main__":
+    root=tk.Tk()
+    game=QuizGUI(root)
+    root.mainloop()
